ca00799683f0add13a7a2b548503890d81d0dfee
Diego Ortín <dieortin@gmail.com>
2019-11-12 04:19:30 +0100

    Modified the database configuration to use the 'ratongato' postgres database

--- ratonGato/settings.py
+++ ratonGato/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,12 +76,15 @@ WSGI_APPLICATION = 'ratonGato.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {'default': dj_database_url.config(
+    default='postgres://alumnodb:alumnodb@localhost:5432/ratongato')}
 
 
 # Password validation
