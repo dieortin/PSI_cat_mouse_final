5edfa2a81253a67526fac162cf4620f35fdb7e2f
Diego Ortín <dieortin@gmail.com>
2019-11-25 21:41:16 +0100

    Now the server checks for an environment variable to initialize in Postgres mode or SQLITE mode

--- ratonGato/settings.py
+++ ratonGato/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,7 +18,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 STATIC_ROOT = "staticfiles"
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -31,7 +29,6 @@ SECRET_KEY = 'mrpv*zn3x01&9bly%l90ftv=hdg9()mi--j8i)a-3pb1w_$+ox'
 DEBUG = True
 
 ALLOWED_HOSTS = [u'afternoon-journey-03892.herokuapp.com', u'127.0.0.1']
-
 
 # Application definition
 
@@ -76,20 +73,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ratonGato.wsgi.application'
 
+print(os.getenv('SQLITE', False))
+if os.getenv('SQLITE', False):
+    # Database
+    # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+    print("Running SQLITE mode")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+else:
+    import dj_database_url
 
-DATABASES = {'default': dj_database_url.config(
-    default='postgres://alumnodb:alumnodb@localhost:5432/ratongato')}
-
+    DATABASES = {'default': dj_database_url.config(
+        default='postgres://alumnodb:alumnodb@localhost:5432/ratongato')}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -109,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -122,7 +122,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
