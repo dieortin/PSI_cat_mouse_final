0c18f4b8abdd3ba774f1e85d7da203424ffb39fc
Diego Ortín <dieortin@gmail.com>
2019-12-12 23:18:39 +0100

    Uncommented a line that heroku needs for collecting static

--- mouse_cat/settings.py
+++ mouse_cat/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
-#STATIC_ROOT = "staticfiles"
+STATIC_ROOT = "staticfiles"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
